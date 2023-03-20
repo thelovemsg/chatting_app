960ac58ee20ac9f1a351b9cf32508f0008c446ef
thelovemsg <thelovemsg@naver.com>
2023-03-20 21:50:50 +0900

    1. api용 공통 ResponseEntity 생성 2. ExceptionHandler를 통한 에러 처리 3. 현재 데이터 insert 테스트 직접 시행중. 4. @Pattern => regexp 오류 수정 5. @NotBlank 적용
    javascript modern deep dive 공부도 해야함 ㅋㅋ

--- socket_jpa_querydsl_test/src/main/java/com/example/socket_jpa_querydsl_test/dto/MemberDto.java
+++ socket_jpa_querydsl_test/src/main/java/com/example/socket_jpa_querydsl_test/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package com.example.socket_jpa_querydsl_test.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,14 +14,20 @@ import org.hibernate.validator.constraints.Length;
 public class MemberDto {
 
     @Email
+    @NotBlank(message = "email needed")
     private String email;
+
+    @NotBlank(message = "name needed")
     private String name;
 
+    @NotBlank
     private String username;
 
     @Pattern(regexp = "\\d+", message = "must contain only digits")
+    @NotBlank(message = "phoneNumber needed")
     private String phoneNumber;
     @Length(min = 8, message = "must be over 8 lengths")
+    @NotBlank(message = "password needed")
     private String password;
 
     private String address1;
