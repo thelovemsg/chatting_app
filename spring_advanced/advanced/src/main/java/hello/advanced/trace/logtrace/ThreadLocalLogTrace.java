c4c9cf6477e587afc4b1a4b478ebaaccc643160c
thelovemsg <thelovemsg@naver.com>
2023-03-28 23:41:00 +0900

    다시 복습중..
    재미있는건 이제와서 다시 보니 나 천재같음...

--- spring_advanced/advanced/src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ spring_advanced/advanced/src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -9,7 +9,7 @@ public class ThreadLocalLogTrace implements LogTrace{
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
-    private static final String EX_PREFIX = "< X-";
+    private static final String EX_PREFIX = "<X-";
 
 //    private TraceId traceIdHolder; // traceId 동기화, 동시성 이슈 발생
     private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
@@ -73,7 +73,7 @@ public class ThreadLocalLogTrace implements LogTrace{
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
-            sb.append( (i == level - 1) ? "|" + prefix : "| ");
+            sb.append( (i == level - 1) ? "|" + prefix : "|  ");
         }
         return sb.toString();
     }
