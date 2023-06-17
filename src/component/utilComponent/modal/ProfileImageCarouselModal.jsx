c91ffe08869086b3314e6586856c540b69d16b61
thelovemsg <thelovemsg@naver.com>
2023-06-17 23:21:19 +0900

    style: apply proper design and image options
    I changed design to let user feel It's almost look like kakao talk.
    And I wanted to rotate, magnifty and shrink image.

--- src/component/utilComponent/modal/ProfileImageCarouselModal.jsx
+++ src/component/utilComponent/modal/ProfileImageCarouselModal.jsx
@@ -2,48 +2,77 @@ import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { GET_TARGET_USER_PROFILE_IMAGE_LIST_REQUEST } from 'reducers/user/userInfoSetting';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faArrowsDownToLine,
+  faMinus,
+  faPlus,
+  faRotate,
+  faEllipsis,
+} from '@fortawesome/free-solid-svg-icons';
 
 const ProfileImageCarouselModal = ({ handleCloseModal, targetUserId }) => {
-  const dispatch = useDispatch();
+  const [rotation, setRotation] = useState(0);
+  const [scale, setScale] = useState(1); // Initial scale is 100% (or normal size)
   const { targetImageList } = useSelector((state) => state.user.info);
+
+  const imageLength = targetImageList.length;
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(GET_TARGET_USER_PROFILE_IMAGE_LIST_REQUEST(targetUserId));
   }, []);
 
-  console.log('targetUserId :: ', targetUserId);
+  const handleShrink = () => {
+    if (scale === 1) return;
+    setScale((prevScale) => Math.max(0.1, prevScale - 0.1)); // Decreases the scale, but not less than 10%
+  };
+
+  const handleMagnify = () => {
+    setScale((prevScale) => Math.min(2, prevScale + 0.1)); // Increases the scale, but not more than 200%
+  };
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const imageRef = useRef([]);
 
-  function onSelectImage({ currentIndex, behavior = 'smooth' }) {
-    setCurrentImageIndex(currentIndex);
-    imageRef.current[currentIndex]?.scrollIntoView({
+  const onSelectImage = (currentIndex) => {
+    setCurrentImageIndex(currentIndex.currentIndex);
+    const currentImage = imageRef.current[currentIndex.currentIndex];
+    currentImage?.scrollIntoView({
       inline: 'center',
       block: 'nearest',
-      behavior,
     });
-  }
+  };
 
-  function handleImageMove(currentIndex) {
+  const handleImageMove = (currentIndex) => {
     const firstIndex = 0;
     const lastIndex = targetImageList.length - 1;
 
     if (currentIndex < firstIndex) {
-      onSelectImage({ currentIndex: lastIndex, behavior: 'auto' });
+      onSelectImage({ currentIndex: lastIndex });
       return;
     }
     if (currentIndex > lastIndex) {
-      onSelectImage({ currentIndex: firstIndex, behavior: 'auto' });
+      onSelectImage({ currentIndex: firstIndex });
       return;
     }
-
     onSelectImage({ currentIndex });
-    // Return value at the end of function
-  }
+    setRotation(0);
+    setScale(1);
+  };
+
+  const handleImageRotation = () => {
+    const nextRotation = (rotation + 90) % 360;
+    setRotation(nextRotation);
+  };
 
   return (
     <>
       <div className="modal-header">
+        <div className="custom-ml-10" style={{ fontSize: '13px' }}>
+          {currentImageIndex + 1}/{imageLength}
+        </div>
         <button
           type="button"
           className="btn-close"
@@ -51,43 +80,93 @@ const ProfileImageCarouselModal = ({ handleCloseModal, targetUserId }) => {
           aria-label="hidden"
         />
       </div>
-      <div
-        className="modal-body"
-        style={{ margin: 'auto', textAlign: 'center', width: '100%' }}
-      >
+      <div className="modal-body">
         <div className="profile-carousel-wrapper">
-          <button
-            type="button"
-            onClick={() => handleImageMove(currentImageIndex - 1)}
-          >
-            previous
-          </button>
-          <div className="profile-image-carousel">
+          {currentImageIndex !== 0 ? (
+            <FontAwesomeIcon
+              className="profile-carousel-image-icon"
+              icon={faAngleLeft}
+              onClick={() => handleImageMove(currentImageIndex - 1)}
+            />
+          ) : (
+            <FontAwesomeIcon
+              className="profile-carousel-image-icon icon-blur"
+              icon={faAngleLeft}
+            />
+          )}
+
+          <div className="profile-image-carousel-box">
             {targetImageList?.length === 0 ? (
               <div>nothing</div>
             ) : (
-              targetImageList.map((user, index) => (
-                <div
-                  ref={(el) => {
-                    imageRef.current[index] = el;
-                  }}
-                >
-                  <img
-                    style={{ width: '300px' }}
-                    alt={`user profile ${index + 1}`}
-                    src={user.avatar}
-                  />{' '}
-                  {/* It should be src instead of scr */}
-                </div>
-              ))
+              targetImageList.map((user, index) => {
+                if (index === currentImageIndex) {
+                  return (
+                    <div
+                      ref={(el) => {
+                        imageRef.current[index] = el;
+                      }}
+                    >
+                      <img
+                        className="profile-image-carousel"
+                        alt={`user profile ${index + 1}`}
+                        src={user.avatar}
+                        style={{
+                          transform: `scale(${scale}) rotate(${rotation}deg)`,
+                        }} // Apply both scale and rotation
+                      />
+                    </div>
+                  );
+                }
+                return null;
+              })
             )}
           </div>
-          <button
-            type="button"
-            onClick={() => handleImageMove(currentImageIndex + 1)}
-          >
-            next
-          </button>
+          {currentImageIndex !== targetImageList.length - 1 ? (
+            <FontAwesomeIcon
+              className="profile-carousel-image-icon"
+              icon={faAngleRight}
+              onClick={() => handleImageMove(currentImageIndex + 1)}
+            />
+          ) : (
+            <FontAwesomeIcon
+              className="profile-carousel-image-icon icon-blur"
+              icon={faAngleRight}
+            />
+          )}
+        </div>
+        <div className="profile-image-carousel-options">
+          <div>
+            <FontAwesomeIcon
+              icon={faMinus}
+              className={`profile-image-icon-option ${
+                scale === 1 ? 'icon-blur' : ''
+              }`}
+              onClick={handleShrink}
+            />
+            <FontAwesomeIcon
+              icon={faPlus}
+              className="profile-image-icon-option"
+              onClick={handleMagnify}
+            />
+            <FontAwesomeIcon
+              icon={faRotate}
+              className="profile-image-icon-option"
+              onClick={() => {
+                handleImageRotation();
+              }}
+            />
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faArrowsDownToLine}
+              className="profile-image-icon-option"
+            />
+            <FontAwesomeIcon
+              icon={faEllipsis}
+              className="profile-image-icon-option"
+            />
+          </div>
         </div>
       </div>
     </>
